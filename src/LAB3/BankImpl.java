3c2e405b6d369731a5d4d99e2e04b46001dc8afd
mm-aditya <aditya.manikashetti@gmail.com>
2017-02-27 01:28:12 +0800

    Lab 3 (SUNDAY version)

---
+++ src/LAB3/BankImpl.java
@@ -0,0 +1,182 @@
+package LAB3;// package Week3;
+
+import com.sun.javafx.image.IntToIntPixelConverter;
+
+public class BankImpl {
+	private int numberOfCustomers;	// the number of customers
+	private int numberOfResources;	// the number of resources
+
+	private int[] available; 	// the available amount of each resource
+	private int[][] maximum; 	// the maximum demand of each customer
+	private int[][] allocation;	// the amount currently allocated
+	private int[][] need;		// the remaining needs of each customer
+
+	public BankImpl (int[] resources, int numberOfCustomers) {
+		// TODO: set the number of resources
+		numberOfResources = resources.length;
+
+		// TODO: set the number of customers
+		this.numberOfCustomers = numberOfCustomers;
+
+		// TODO: set the value of bank resources to available
+		available = resources;
+
+		// TODO: set the array size for maximum, allocation, and need
+		maximum = new int[this.numberOfCustomers][numberOfResources];
+		allocation = new int[this.numberOfCustomers][numberOfResources];
+		need = new int[this.numberOfCustomers][numberOfResources];
+
+	}
+
+	public int getNumberOfCustomers() {
+		// TODO: return numberOfCustomers
+		return numberOfCustomers;
+	}
+
+	public void addCustomer(int customerNumber, int[] maximumDemand) {
+		// TODO: add customer, update maximum and need
+		maximum[customerNumber] = maximumDemand;
+		for(int i = 0; i < numberOfResources;i++) {
+			need[customerNumber][i] = maximum[customerNumber][i];
+		}
+	}
+
+	public void getState() {
+		// TODO: print the current state with a tidy format
+		System.out.println("--------Current state of the bank------------\n");
+
+		// TODO: print available
+		System.out.println("Available:");
+		for(int i =0;i<numberOfResources;i++)
+			System.out.print(String.format("%4d",available[i]));
+		System.out.println("\n");
+
+		// TODO: print maximum
+		System.out.println("Maximum:");
+		for(int i =0; i < numberOfCustomers;i++){
+			for(int k=0;k<numberOfResources;k++)
+				System.out.print(String.format("%4d",maximum[i][k]));
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+
+		// TODO: print allocation
+		System.out.println("Allocation:");
+		for(int i =0; i < numberOfCustomers;i++){
+			for(int k=0;k<numberOfResources;k++)
+				System.out.print(String.format("%4d",allocation[i][k]));
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+
+		// TODO: print need
+		System.out.println("Need:");
+		for(int i =0; i < numberOfCustomers;i++){
+			for(int k=0;k<numberOfResources;k++)
+				System.out.print(String.format("%4d",need[i][k]));
+			System.out.print("\n");
+		}
+		System.out.print("\n");
+
+	}
+
+	public synchronized boolean requestResources(int customerNumber, int[] request) {
+		// TODO: print the request
+		System.out.println("Request is: ");
+		for(int i = 0; i < request.length;i++)
+			System.out.print(String.format("%4d",request[i]));
+		System.out.println("\n");
+
+		// TODO: check if request larger than need
+		if(lessThan(need[customerNumber],request))
+			return false;
+		System.out.println("need smaller than request!");
+
+		// TODO: check if request larger than available
+		if(lessThan(available,request))
+			return false;
+		System.out.println("Available smaller than request");
+
+		// TODO: check if the state is safe or not
+		if(!checkSafe(customerNumber,request))
+			return false;
+
+		System.out.println("allocatin");
+		// TODO: if it is safe, allocate the resources to customer customerNumber
+		for(int i =0; i < numberOfResources;i++) {
+			allocation[customerNumber][i] = request[i];
+			need[customerNumber][i] = maximum[customerNumber][i] - allocation[customerNumber][i];
+		}
+		return true;
+	}
+
+	public synchronized void releaseResources(int customerNumber, int[] release) {
+		// TODO: print the release
+		// release the resources from customer customerNumber
+		System.out.println("Release requested is: ");
+		for(int i = 0; i < release.length;i++)
+			System.out.print(String.format("%4d",release[i]));
+
+		if(lessThan(need[customerNumber],release))
+			System.out.println("Cannot release!");
+		else {
+			for (int i = 0; i < numberOfResources; i++)
+				allocation[customerNumber][i] = allocation[customerNumber][i] - release[i];
+		}
+	}
+
+	private synchronized boolean checkSafe(int customerNumber, int[] request) {
+		// TODO: check if the state is safe
+		int[] curravailable = new int[numberOfResources];
+		int[][] currneed = need.clone();
+		int[][] currallocation = allocation.clone();
+		boolean[] finish = new boolean[numberOfCustomers];
+		for(int i = 0; i < numberOfCustomers;i++)
+			finish[i] = false;
+		boolean possible = true;
+
+
+		for(int i =0;i<numberOfResources;i++)
+			curravailable[i] = available[i] - request[i];
+
+		for(int i =0;i<numberOfResources;i++)
+			currneed[customerNumber][i] = need[customerNumber][i] - request[i];
+
+		for(int i =0;i<numberOfResources;i++) {
+			currallocation[customerNumber][i] = allocation[customerNumber][i] + request[i];
+		}
+
+		int[] work = curravailable;
+
+		while(possible){
+			//System.out.println("running this!");
+			//System.out.println(numberOfCustomers);
+			possible = false;
+			for(int i =0; i < numberOfCustomers;i++){
+				if(finish[i] == false && lessThan(currneed[i],work)){
+					possible = true;
+					for(int k =0; k < numberOfResources;k++)
+						work[k]+=currallocation[i][k];
+					finish[i] = true;
+				}
+			}
+		}
+
+		boolean res = true;
+		for(int i = 0; i < numberOfCustomers;i++){
+			if(finish[i] == false)
+				res = !res;
+		}
+		return res;
+
+	}
+
+	private boolean lessThan(int[] a, int[] b){
+		boolean res = true;
+		for(int i = 0; i <a.length;i++){
+			if(a[i] > b[i])
+				res = !res;
+		}
+		return res;
+	}
+}
---
+++ src/Misc.java
@@ -0,0 +1,9 @@
+/**
+ * Created by aditya on 26/2/2017.
+ */
+public class Misc {
+    public static void main(String[] args) {
+        String output = String.format("%3d%3d%3d",1,12,123);
+        System.out.println(output);
+    }
+}
